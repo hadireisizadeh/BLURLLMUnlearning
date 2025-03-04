1a2f80e2d9770f039074fd280bcd4e0503a3d43f
hadireisizadeh <hadi.reisizadeh@gmail.com>
2025-03-04 15:16:11 -0600

    Add files via upload

---
+++ MUSE/books_eval.sh
@@ -0,0 +1,9 @@
+python eval.py \
+  --model_dirs "/out_dir/books/BLO_forget_lower_npo_gdr/checkpoint-139" \
+  --names "checkpoint-139" \
+  --corpus "books" \
+  --out_file "/result/out.csv" \
+  --temp_dir "/temp"
+
+
+  
---
+++ MUSE/news_eval.sh
@@ -0,0 +1,9 @@
+python eval.py \
+  --model_dirs "/out_dir/news/BLO_forget_lower_npo_gdr/checkpoint-102" \
+  --names "checkpoint-102" \
+  --corpus "news" \
+  --out_file "/result/out.csv" \
+  --temp_dir "/temp"
+
+
+  
